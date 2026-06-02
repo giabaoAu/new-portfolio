5b1967fe1a9b7e4376a0fe0dc5cc10aee46f3bea
giabaoAu <baoau135@gmail.com>
2026-06-02 22:56:17 +0930

    Fixed macbook screen scrolling issue on laptop screen size

--- src/components/ui/macbook-scroll.tsx
+++ src/components/ui/macbook-scroll.tsx
@@ -68,7 +68,7 @@ export const MacbookScroll = ({
   return (
     <div
       ref={ref}
-      className="flex min-h-[200vh] shrink-0 scale-[0.35] transform flex-col items-center justify-start py-0 [perspective:800px] sm:scale-50 md:scale-100 pt-30 pb-80"
+      className="flex min-h-[150vh] shrink-0 scale-[0.35] transform flex-col items-center justify-start py-0 [perspective:800px] sm:scale-50 md:scale-100 pt-30 pb-80"
     >
       <motion.h2
         style={{
